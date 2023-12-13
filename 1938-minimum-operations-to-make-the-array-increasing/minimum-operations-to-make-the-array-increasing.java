a5eea7a5d9fa60749bc121928f1a7bc6c35d45f8
Swayam Badhe <123235851+SwayamBadhe@users.noreply.github.com>
2023-12-13 23:25:19 +0530

    Time: 4 ms (53.33%) | Memory: 44.7 MB (5.70%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -3,10 +3,9 @@ class Solution {
         int cnt = 0;
         for (int i=0; i!=(nums.length) - 1; i++) {
             if (nums[i] >= nums[i+1]) {
-                int diff = nums[i] - nums[i+1];
-                nums[i+1] += diff + 1;
-                cnt += diff + 1;
-            }
+                cnt += nums[i] - nums[i+1] + 1;
+                nums[i+1] = nums[i] + 1;                
+            }            
         }
         return cnt;
     }
