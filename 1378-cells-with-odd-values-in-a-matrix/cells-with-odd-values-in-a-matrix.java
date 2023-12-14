9ce83216f6f740bdfc23d129a5faadb1ad07f68d
Swayam Badhe <123235851+SwayamBadhe@users.noreply.github.com>
2023-12-14 16:57:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (29.12%) - LeetSync

--- 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
+++ 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.java
@@ -1,22 +1,43 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        int[][] matrix = new int[m][n];
+        // int[][] matrix = new int[m][n];
+        // for (int i=0; i!=indices.length; i++) {
+        //     int rowVal = indices[i][0];
+        //     int colVal = indices[i][1];
+        //     for (int j=0; j!=n; j++) {
+        //         matrix[rowVal][j]++;
+        //     }
+        //     for (int j=0; j!=m; j++) {
+        //         matrix[j][colVal]++;
+        //     }
+        // }
+        // int oddD = 0;
+        // for (int[] row: matrix) {
+        //     for (int val: row) {
+        //         if (val % 2 == 1) oddD++; 
+        //     }
+        // }
+        // return oddD;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
         for (int i=0; i!=indices.length; i++) {
             int rowVal = indices[i][0];
             int colVal = indices[i][1];
-            for (int j=0; j!=n; j++) {
-                matrix[rowVal][j]++;
-            }
-            for (int j=0; j!=m; j++) {
-                matrix[j][colVal]++;
-            }
+            row[rowVal] ^= true;
+            col[colVal] ^= true;
         }
-        int oddD = 0;
-        for (int[] row: matrix) {
-            for (int val: row) {
-                if (val % 2 == 1) oddD++; 
-            }
+
+        int r = 0, c = 0;
+        for (int i=0; i!=m; i++) {
+            if (row[i]) r++;
         }
-        return oddD;
+        for (int i=0; i!=n; i++) {
+            if (col[i]) c++;
+        }
+
+        return r*n + c*m - 2*r*c;
+
+
     }
 }
