31c747eec5532e0fd9df6f6168e4a1699897c4e2
Swayam Badhe <123235851+SwayamBadhe@users.noreply.github.com>
2023-12-24 12:04:19 +0530

    Time: 1 ms (100.00%) | Memory: 47.9 MB (5.08%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,20 +1,40 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = nums[i] * nums[i];
-        }
-        int n = nums.length;
-        for (int i = 0; i < n-1; i++)
-        {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (nums[j] < nums[min_idx])
-                    min_idx = j;
+        int left = 0;
+        int right = nums.length - 1;
+        int index = nums.length - 1;
 
-            int temp = nums[min_idx];
-            nums[min_idx] = nums[i];
-            nums[i] = temp;
+        int result[] = new int[nums.length];
+
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+            index--;        
         }
-    return nums;
+        return result;
     }
+
+
+    //     for (int i = 0; i < nums.length; i++) {
+    //         nums[i] = nums[i] * nums[i];
+    //     }
+    //     int n = nums.length;
+    //     for (int i = 0; i < n-1; i++)
+    //     {
+    //         int min_idx = i;
+    //         for (int j = i+1; j < n; j++)
+    //             if (nums[j] < nums[min_idx])
+    //                 min_idx = j;
+
+    //         int temp = nums[min_idx];
+    //         nums[min_idx] = nums[i];
+    //         nums[i] = temp;
+    //     }
+    // return nums;
+    // }
 }
