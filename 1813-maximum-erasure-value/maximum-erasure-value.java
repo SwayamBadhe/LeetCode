1f22c91e24e8ffcdc42b0e11832f4776d0fc87be
Swayam Badhe <123235851+SwayamBadhe@users.noreply.github.com>
2025-02-09 16:00:03 +0530

    Time: 47 ms (78.09%) | Memory: 56.8 MB (92.49%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -7,15 +7,16 @@ class Solution {
 
         for (int i=0; i!=nums.length; i++) {
             int num = nums[i];
-            if (numsMap.containsKey(num) && numsMap.get(num) > minSoFar) {
-                for (int j=minSoFar; j!=numsMap.get(num); j++) {
-                    currSum -= nums[j];
-                }
-                minSoFar = Math.max(minSoFar, numsMap.get(num));            
+            if (numsMap.containsKey(num) && numsMap.get(num) >= minSoFar) {
+                int prev = numsMap.get(num);
+                for (int j=minSoFar; j<=prev; j++) {
+                    currSum -= nums[minSoFar];
+                    minSoFar++;
+                }           
             } 
             currSum += num;
             maxSum = Math.max(maxSum, currSum);
-            numsMap.put(num, i + 1); 
+            numsMap.put(num, i); 
         }
         return maxSum;
     }
